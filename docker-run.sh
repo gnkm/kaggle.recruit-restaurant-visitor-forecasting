e43c0395fb753205222863640cfdd5d46638ba15
Genki Matsunaga <genki.matsunaga@gmail.com>
2020-10-04 09:54:11 +0900

    Edit docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -10,14 +10,14 @@ ____USAGE
 
 # main script starts here
 
-if [ $1 = 'py' ]; then
+if [ $1 = 'run' ]; then
     docker run \
         -v $PWD:/tmp/working \
         -w=/tmp/working \
         --rm \
         -it \
         --name kaggle-python \
-        gcr.io/kaggle-images/python \
+        gcr.io/kaggle-images/python@sha256:af3e19d6210754b09d3ae97d30210712ff183877ab6d0fa71485a0dd7c1f33b3 \
         python $2
 elif [ $1 = 'jp' ]; then
     docker run \
@@ -27,10 +27,37 @@ elif [ $1 = 'jp' ]; then
         --rm \
         -it \
         --name kaggle-jupyter \
-        gcr.io/kaggle-images/python \
+        gcr.io/kaggle-images/python@sha256:af3e19d6210754b09d3ae97d30210712ff183877ab6d0fa71485a0dd7c1f33b3 \
         jupyter notebook \
             --no-browser \
             --ip=0.0.0.0 \
             --allow-root \
             --notebook-dir=/tmp/working
+elif [ $1 = 'flake' ]; then
+    docker run \
+        -v $PWD:/tmp/working \
+        -w=/tmp/working \
+        --rm \
+        -it \
+        --name kaggle-python \
+        gcr.io/kaggle-images/python@sha256:af3e19d6210754b09d3ae97d30210712ff183877ab6d0fa71485a0dd7c1f33b3 \
+        flake8 $2
+elif [ $1 = 'scripts' ]; then
+    docker run \
+        -v $PWD:/tmp/working \
+        -w=/tmp/working \
+        --rm \
+        -it \
+        --name kaggle-python \
+        gcr.io/kaggle-images/python@sha256:af3e19d6210754b09d3ae97d30210712ff183877ab6d0fa71485a0dd7c1f33b3 \
+        python $2 $3
+else
+    docker run \
+        -v $PWD:/tmp/working \
+        -w=/tmp/working \
+        --rm \
+        -it \
+        --name kaggle-python \
+        gcr.io/kaggle-images/python@sha256:af3e19d6210754b09d3ae97d30210712ff183877ab6d0fa71485a0dd7c1f33b3 \
+        $1
 fi
